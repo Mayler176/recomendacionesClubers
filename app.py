416a5ea7236dc21d0916e19ffdab44743232116a
Ahha <62366280+Mayler176@users.noreply.github.com>
2025-06-10 03:05:14 +0000

    quitando rayitas y puntos

--- app.py
+++ app.py
@@ -97,26 +97,6 @@ def plot_preference_pie_nivo(client_id):
                 arcLabelsSkipAngle=0,  # para asegurar que no aparezcan estáticas
                 arcLabelsTextColor="transparent",  # oculta texto en secciones
                 theme=dark_theme,
-                defs=[
-                    {
-                        "id": "dots",
-                        "type": "patternDots",
-                        "background": "inherit",
-                        "color": "rgba(255, 255, 255, 0.3)",
-                        "size": 4,
-                        "padding": 1,
-                        "stagger": True,
-                    },
-                    {
-                        "id": "lines",
-                        "type": "patternLines",
-                        "background": "inherit",
-                        "color": "rgba(255, 255, 255, 0.3)",
-                        "rotation": -45,
-                        "lineWidth": 6,
-                        "spacing": 10,
-                    },
-                ],
                 fill=[
                     {"match": {"id": renombradas.index[0]}, "id": "dots"},
                     {"match": {"id": renombradas.index[1]}, "id": "lines"},
